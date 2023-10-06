ae5342dc2152188ffed477f815cde8f53252ac71
student <student@gnu.ac.in>
2023-10-06 12:32:30 +0530

    okok

--- app/src/main/java/com/example/mad_practical_9_21012021051/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021051/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_9_21012021051
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,20 +8,34 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     lateinit var alarmanimation: AnimationDrawable
+   lateinit var heartanimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val alarmimage = findViewById<ImageView>(R.id.Alarmimage)
         alarmimage.setBackgroundResource(R.drawable.alarm_animation_list)
         alarmanimation =alarmimage.background as AnimationDrawable
+
+        val heartimage = findViewById<ImageView>(R.id.Heartimage)
+        heartimage.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation =heartimage.background as AnimationDrawable
+
     }
+
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         if (hasFocus)
-        {alarmanimation.start()}
-        else{
-            alarmanimation.stop()
+        {heartanimation.start();alarmanimation.start()
+
         }
+        else{
+            heartanimation.stop();alarmanimation.stop()
+        }
+
         super.onWindowFocusChanged(hasFocus)
     }
+
+
+
 }
