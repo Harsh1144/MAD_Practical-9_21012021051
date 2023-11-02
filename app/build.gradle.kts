28421a5f4787c70b2eade34e3d8a1669df8098ee
strudent <student@gnu.ac.in>
2023-11-02 22:38:30 +0530

    hehehe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mad_practical_9_21012021051"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mad_practical_9_21012021051"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/mad_practical_9_21012021051/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021051/SplashActivity.kt
@@ -1,46 +1,52 @@
 package com.example.mad_practical_9_21012021051
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
-import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity(),AnimationListener {
-    lateinit var uvpceanimation: AnimationDrawable
-    @SuppressLint("MissingInflatedId")
+class SplashActivity : AppCompatActivity() {
+    lateinit var logoanimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val uvpceimage = findViewById<ImageView>(R.id.uvpce)
-        uvpceimage.setBackgroundResource(R.drawable.uvpce_animation)
-        uvpceanimation =uvpceimage.background as AnimationDrawable
+        val logo: ImageView = findViewById(R.id.uvpce)
+
+        logo.setBackgroundResource(R.drawable.uvpce_animation)
+        logoanimation = logo.background as AnimationDrawable;
+
+        val myAnimation = AnimationUtils.loadAnimation(this, R.anim.tween_animation)
+        logo.startAnimation(myAnimation)
+
+        // Add an animation listener to your animation
+        myAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+                // Animation started
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                // Animation ended, start a new activity here
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+                // Animation repeated
+            }
+        })
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
-        if (hasFocus)
-        {
-            uvpceanimation.start()
-        }
-        else{
-           uvpceanimation.stop()
-        }
-
         super.onWindowFocusChanged(hasFocus)
-    }
-
-    override fun onAnimationStart(p0: Animation?) {
-
-    }
-
-    override fun onAnimationEnd(p0: Animation?) {
-
-    }
-
-    override fun onAnimationRepeat(p0: Animation?) {
-
+        if (hasFocus) {
+            logoanimation.start()
+        } else {
+            logoanimation.stop()
+        }
     }
 }
